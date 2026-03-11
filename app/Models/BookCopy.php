41035d5390b6200f99f41323d1991230897076c7
𝚂𝙰𝙷𝙷𝙾𝚄𝚃𝙸 𝙰𝙼𝙸𝙽𝙴 <midoriaduko@gmail.com>
2026-03-11 01:38:30 +0100

    feat: create BookCopy model for managing individual book instances and conditions

---
+++ app/Models/BookCopy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BookCopy extends Model
+{
+    protected $fillable = ['book_id', 'condition', 'status'];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function borrows()
+    {
+        return $this->hasMany(Borrow::class);
+    }}
